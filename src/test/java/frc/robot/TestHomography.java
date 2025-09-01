40ea294ac26ce9edd1329de26ba56e2780d7603e
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2025-09-01 01:26:59 -0500

    Somewhat close to homograpy but still not there yet

--- src/test/java/frc/robot/TestHomography.java
+++ src/test/java/frc/robot/TestHomography.java
@@ -24,7 +24,8 @@ public class TestHomography {
             new Point(764, 461), new Point(194, 342.5), new Point(138.9, 233.3), new Point(329.9, 336.7));
     private MatOfPoint2f world = new MatOfPoint2f(new Point(0.57, -0.18), new Point(0.65, -0.51), new Point(4, 0.88),
             new Point(4, -2.86), new Point(1, -1), new Point(2, -1));
-    private static Mat world2Robot = new Mat(4, 4, CvType.CV_64F, Scalar.all(0));
+
+    private static Mat world2Robot = Mat.eye(4, 4, CvType.CV_64F);
     private static Mat robot2Camera = new Mat(4,4, CvType.CV_64F, Scalar.all(0));
     private static Mat wpi2Opencv = new Mat(4,4, CvType.CV_64F, Scalar.all(0));
 
@@ -34,10 +35,6 @@ public class TestHomography {
         world2Robot.put(0, 3, -0.381);   // forward offset in x
         world2Robot.put(1, 3, -0.381);   // left offset in y
         world2Robot.put(2, 3, -0.206);   // height of the algae center
-        world2Robot.put(0, 0, 1.0); // No rotation so it's identity
-        world2Robot.put(1, 1, 1.0);
-        world2Robot.put(2, 2, 1.0);
-        world2Robot.put(3, 3, 1.0); // The homogeneous one
 
         // Initialize our homogenius robot to the camera transformation from Camera.robotToCamera
         robot2Camera.put(0,0, Camera.robotToCamera.toMatrix().getData());
@@ -47,11 +44,6 @@ public class TestHomography {
             new edu.wpi.first.math.geometry.Translation3d(0,0,0),
             new edu.wpi.first.math.geometry.Rotation3d(-Math.PI/2, 0, -Math.PI/2));
         wpi2Opencv.put(0, 0, tWpi2Opencv.toMatrix().getData());
-
-        // System.out.println("World to Robot Matrix:\n" + world2Robot.dump());
-        // System.out.println("Robot to Camera Matrix:\n" + robot2Camera.dump());
-        // System.out.println("World to Camera Matrix:\n" + world2Robot.matMul(robot2Camera).dump());
-        // System.out.println("WPI to OpenCV Matrix:\n" + opencv2wpi.dump());
     }
 
     String matToString(Mat m) {
@@ -94,6 +86,17 @@ public class TestHomography {
         return sb.toString();
     }
 
+    Mat reduceHomo(Mat h) {
+        // Reduce a homogeneous matrix so that the bottom-right value is 1.0
+        Mat r = h.clone().rowRange(0, h.rows()-1);
+        for(int i=0; i < r.cols(); i++) {
+            double scale = h.get(h.rows()-1, i)[0];
+            if (scale == 0.0) scale = 1.0;
+            Core.divide(r.colRange(i, i+1), new Scalar(scale), r.colRange(i, i+1));
+        }
+        return r;
+    }
+
     @Test
     void testGeneralHomography() {
         MatOfPoint2f imagePoints = new MatOfPoint2f(new Point(400,240));
@@ -133,34 +136,57 @@ public class TestHomography {
         // Same chain of transformations but with one matrix
         Mat comboMat = world2Robot.matMul(robot2Camera).matMul(wpi2Opencv);
         System.out.println("Combo Matrix:\n" + matToString(comboMat));
-        Mat projectionMat = Mat.zeros(3, 3, CvType.CV_64F);
-        projectionMat.put(0, 0, comboMat.get(2, 3));
-        projectionMat.put(1, 1, comboMat.get(2, 3));
-        projectionMat.put(2, 2, 1);
+        Mat camToWorldTranslation = Mat.eye(4, 4, CvType.CV_64F);
+        // projectionMat.put(0, 0, comboMat.get(2, 3));
+        // projectionMat.put(1, 1, comboMat.get(2, 3));
+        camToWorldTranslation.put(0, 3, -comboMat.get(0, 3)[0]);
+        camToWorldTranslation.put(1, 3, -comboMat.get(1, 3)[0]);
+        // projectionMat.put(2, 2, 1.0/comboMat.get(2, 3)[0]);
+        System.out.println("Cam to World Matrix:\n" + matToString(camToWorldTranslation));
+        System.out.println("World to cam Matrix:\n" + matToString(camToWorldTranslation.inv()));
+
+        double f = comboMat.get(2, 3)[0];
+        System.out.println("Focal length (from combo matrix): " + f);
 
         // Compare our "theoretical" projections to the actual measured points
         assert image.rows() == world.rows() : "Image and world must have the same number of points";
         for (int i=0; i < image.rows(); i++) {
-            Point wP2d = world.toList().get(i);
-            Point aP2d = image.toList().get(i);
-            Mat iPoint3d = new Mat(3, 1, CvType.CV_64F, new Scalar(0));
-            iPoint3d.put(0, 0, aP2d.x);
-            iPoint3d.put(1, 0, aP2d.y);
-            iPoint3d.put(2, 0, 1.0); // Homogeneous coordinate
+            Point wPoint2d = world.toList().get(i);
+            Point aPoint2d = image.toList().get(i);
+            System.out.println("\nPoint " + i + " Image: " + aPoint2d + " World: " + wPoint2d);
 
-            Mat cPoint3d = cameraInv.matMul(iPoint3d);
+            Mat aPoint3d = Mat.ones(3, 1, CvType.CV_64F);
+            aPoint3d.put(0, 0, aPoint2d.x);
+            aPoint3d.put(1, 0, aPoint2d.y);
+            Mat inCamCoord3d = cameraInv.matMul(aPoint3d);
 
-            Mat cPoint4d = new Mat(4, 1, CvType.CV_64F, new Scalar(0));
-            cPoint4d.put(0, 0, cPoint3d.get(0, 0)[0]);
-            cPoint4d.put(1, 0, cPoint3d.get(1, 0)[0]);
-            cPoint4d.put(2, 0, cPoint3d.get(2, 0)[0]);
-            cPoint4d.put(3, 0, 1.0); // Homogeneous coordinate
+            Mat homoInCam4d = Mat.ones(4, 1, CvType.CV_64F);
+            homoInCam4d.put(0, 0, inCamCoord3d.get(0, 0)[0]);
+            homoInCam4d.put(1, 0, inCamCoord3d.get(1, 0)[0]);
+            homoInCam4d.put(2, 0, inCamCoord3d.get(2, 0)[0]);
+            System.out.println("Point " + i + " in Camera coords: " + matToString(homoInCam4d));
 
-            Mat wPoint4d = comboMat.matMul(cPoint4d);
-            Point wP3d = new Point(wPoint4d.get(0, 0)[0], wPoint4d.get(1, 0)[0]);
-            System.out.println("    Actual Point: " + wP2d);
-            System.out.println("Calculated Point: " + wP3d + " Z: " + wPoint4d.get(2, 0)[0]);
-            System.out.println("Homo point: " + matToString(cPoint4d));
+            Mat homoCamInWpi4d = wpi2Opencv.matMul(homoInCam4d);
+            System.out.println("Point " + i + " in WPI coords: " + matToString(homoCamInWpi4d));
+
+            Mat homoInRobot4d = robot2Camera.matMul(homoCamInWpi4d);
+            System.out.println("Point " + i + " in Robot coords: " + matToString(homoInRobot4d));
+
+            Mat homoInWorld4d = world2Robot.matMul(homoInRobot4d);
+            System.out.println("Point " + i + " in World coords: " + matToString(homoInWorld4d));
+
+            // All above in one step
+            // Mat wPoint4d = comboMat.matMul(homoInCam4d);
+            // Mat reduced = reduceHomo(wPoint4d);
+
+            Mat homoShifted = camToWorldTranslation.matMul(homoInWorld4d);
+            double z = homoInWorld4d.get(2, 0)[0];
+            double scale = f / (f - z);
+            Mat homoProjected = new Mat(4, 1, CvType.CV_64F);
+            Core.multiply(homoShifted, new Scalar(scale), homoProjected);
+            Mat homoUnshifted = camToWorldTranslation.inv().matMul(homoProjected);
+            System.out.println("Point " + i + " projected: " + matToString(homoUnshifted));
+            System.out.println("Scale: " + scale);
         }
 
         // Test the artificial homography matrix
