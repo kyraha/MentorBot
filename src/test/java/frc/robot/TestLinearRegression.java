1f95d83c72f2f44fe96e898fccd68bcf7947d1dc
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2025-09-06 23:33:03 -0500

    Upside down variant; plus description of the theory

--- src/test/java/frc/robot/TestLinearRegression.java
+++ src/test/java/frc/robot/TestLinearRegression.java
@@ -3,57 +3,81 @@ package frc.robot;
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.junit.jupiter.api.Test;
 
-import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.numbers.N3;
 
 public class TestLinearRegression {
+
+    /**
+     * This test demonstrates how find camera rotation using linear regression by making multiple
+     * measurements of 3D points and then derive the rotation needed to align the camera's "down"
+     * direction with the horizontal plane normal.
+     * 
+     * The plane equation is of the form: ax + by + cz + d = 0 (in camera coordinates)
+     * For our purposes, we fix c = -1, so the equation becomes: ax + by - z + d = 0
+     * Rearranging gives: z = d + ax + by
+     * 
+     * We can use ordinary least squares regression to find the coefficients a, b, and d.
+     * 
+     * Once we have the plane normal vector (a, b, -1), we can compute the rotation
+     * needed to align the camera's "down" vector (0, 0, -1) with the plane normal.
+     */
     @Test
     void test() {
-        // The plane is defined by the equation ax + by + cz + d = 0,
-        // where (a, b, c) is a normal vector
-        // and d = -(ax0 + by0 + cz0) for some point (x0, y0, z0) on the plane.
-        // Assume c = -1, then a regression equation is z = d + ax + by
-        // and [d, a, b] are the regression parameters.
-
-        // Pretend we collected a bunch of sample data points (x, y, z) with getBestCameraToTarget()
+        // Pretend we collected a bunch of sample data points (x, y, z)
+        // In reality, just drive around the same AprilTag and record a few getBestCameraToTarget()
         double[] z = {
-            0,
-            1,
-            2
+            -0.0533,
+            -0.343,
+            -0.822
         };
         double[][] xy = {
-            {2, 0},
-            {4, 1},
-            {4, -1}
+            {0.487, -0.1498},
+            {3.45, 1.39},
+            {2.94, -1.295}
         };
 
         // Create a regression model and fit the data
         OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
         regression.newSampleData(z, xy);
-        double[] coefficients = regression.estimateRegressionParameters();
-        System.out.println(
+        System.out.println( // Just to see how good the fit is
             String.format("Standard Error: % .4f; R^2: % .4f",
             regression.estimateRegressionStandardError(),
             regression.calculateRSquared()));
 
+        double[] coefficients = regression.estimateRegressionParameters();
         double d = coefficients[0];
         double a = coefficients[1];
         double b = coefficients[2];
-        double c = -1.0; // c is fixed to -1
-        System.out.println(String.format("Plane equation: % .4fx + % .4fy + % .4fz + % .4f = 0", a, b, c, d));
+        double c = -1.0; // c is fixed to -1 (why? read above)
+        System.out.println(String.format("Plane equation: %.3fx %+.3fy %+.3fz %+.3f = 0", a, b, c, d));
 
-        Vector<N3> cameraNormal = new Vector<N3>(Nat.N3());
-        cameraNormal.set(0, 0, 0);
-        cameraNormal.set(1, 0, 0);
-        cameraNormal.set(2, 0, -1);
-        Vector<N3> planeNormal = new Vector<N3>(Nat.N3());
-        planeNormal.set(0, 0, a);
-        planeNormal.set(1, 0, b);
-        planeNormal.set(2, 0, c);
-        Rotation3d rotation = new Rotation3d(planeNormal, cameraNormal);
-        System.out.println(String.format("Roll: %f, Pitch: %f, Yaw: %f, Angle: %f", rotation.getX(), rotation.getY(), rotation.getZ(), rotation.getAngle()));
-        System.out.println(rotation.getQuaternion().toString());
+        // The plane normal vector from the camera POV is (a, b, c)
+        Vector<N3> planeNormal = VecBuilder.fill(a, b, c);
+
+        // Camera's own "down" direction is simply (0, 0, -1)
+        Vector<N3> cameraNormal = VecBuilder.fill(0, 0, -1);
+
+        // Camera is rotated as if plane would be rotated to match the camera normal
+        // i.e, the rotation direction is from true "down" (plane normal) to camera "down" (camera normal)
+        Rotation3d rotation1 = new Rotation3d(planeNormal, cameraNormal);
+        System.out.println(String.format("If right side up. Roll = %.3f, Pitch = %.3f, Yaw = %.3f\n  %s",
+            rotation1.getX(),
+            rotation1.getY(),
+            rotation1.getZ(),
+            rotation1.getQuaternion()));
+
+        // Alternatively, if the camera is upside down, the camera normal would point "up"
+        cameraNormal.set(2, 0, 1); // Set Z component to +1
+        Rotation3d rotation2 = new Rotation3d(planeNormal, cameraNormal);
+        System.out.println(String.format("If upside down. Roll = %.3f, Pitch = %.3f, Yaw = %.3f\n  %s",
+            rotation2.getX(),
+            rotation2.getY(),
+            rotation2.getZ(),
+            rotation2.getQuaternion()));
+
+        // However, yaw here is fantom and must be adjusted further (to be relative to what?)
     }
 }
